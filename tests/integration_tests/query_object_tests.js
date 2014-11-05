75994bbbe8d1194d23dd135429f3c8515ced7a8a
Owen Evans <owen@bgeek.net>
2014-11-06 12:33:52 +1300

    moar tests

--- tests/integration_tests/query_object_tests.js
+++ tests/integration_tests/query_object_tests.js
@@ -15,59 +15,57 @@ describe('integration', function () {
   var clock;
   before(function () {
     clock = sinon.useFakeTimers(new Date().getTime());
+    setContext = function () {
+      return hoistContext.get().then(function (context) {
+        context.bucket = bucket;
+        context.member = member;
+        context.application = application;
+        context.roles = roles;
+        return context;
+      });
+    };
   });
   after(function () {
     clock.restore();
   });
+  var pipeline = require('../../lib/pipeline')(hoistContext);
+  var application = new Application({
+    _id: 'applicationid',
+    dataKey: 'datakey',
+    anonymousPermissions: {
+      live: []
+    }
+  });
+  var bucket = new Bucket({
+    _id: 'bucketid',
+    application: application._id,
+    environment: 'live'
+  });
+  var reader = new Role({
+    _id: 'readerRoleId',
+    application: application._id,
+    environment: 'live',
+    claims: ['DataRead']
+  });
+  var globalReader = new Role({
+    _id: 'globalReaderRoleId',
+    application: application._id,
+    environment: 'live',
+    claims: ['DataGlobalRead']
+  });
+  var member = new Member({
+    _id: 'memberid',
+    application: 'application',
+    environment: 'live',
+    emailAddresses: [{
+      address: 'owen@hoist.io',
+      verified: true
+    }],
+    roles: {}
+  });
+  var setContext;
+  var roles = [reader, globalReader];
   describe('findOne', function () {
-    var pipeline = require('../../lib/pipeline')(hoistContext);
-    var application = new Application({
-      _id: 'applicationid',
-      dataKey: 'datakey',
-      anonymousPermissions: {
-        live: []
-      }
-    });
-    var bucket = new Bucket({
-      _id: 'bucketid',
-      application: application._id,
-      environment: 'live'
-    });
-    var reader = new Role({
-      _id: 'readerRoleId',
-      application: application._id,
-      environment: 'live',
-      claims: ['DataRead']
-    });
-    var globalReader = new Role({
-      _id: 'globalReaderRoleId',
-      application: application._id,
-      environment: 'live',
-      claims: ['DataGlobalRead']
-    });
-    var member = new Member({
-      _id: 'memberid',
-      application: 'application',
-      environment: 'live',
-      emailAddresses: [{
-        address: 'owen@hoist.io',
-        verified: true
-      }],
-      roles: {}
-    });
-    var setContext;
-    var roles = [reader, globalReader];
-    before(function () {
-      setContext = function () {
-        return hoistContext.get().then(function (context) {
-          context.bucket = bucket;
-          context.member = member;
-          context.application = application;
-          context.roles = roles;
-          return context;
-        });
-      };
-    });
     describe('in a bucket', function () {
       before(function () {
         var _original = setContext;
@@ -369,7 +367,7 @@ describe('integration', function () {
                 return db.dropDatabase();
               });
           });
-          it('returns NULL',function(){
+          it('returns NULL', function () {
             return expect(_result).to.be.null;
           });
         });
@@ -382,20 +380,20 @@ describe('integration', function () {
             role: reader._id
           }];
           hoistContext.namespace.run(function () {
-              setContext().then(function () {
-                pipeline.findOne('person', {
+            setContext().then(function () {
+              pipeline.findOne('person', {
 
-                }).nodeify(function (err) {
-                  error = err;
-                  done();
-                });
+              }).nodeify(function (err) {
+                error = err;
+                done();
               });
             });
+          });
         });
         after(function () {
           member.roles = {};
         });
-        it('throws permission error',function(){
+        it('throws permission error', function () {
           expect(error.message).to.eql('Current user does not have permission to Read Global Data');
         });
       });
@@ -403,9 +401,62 @@ describe('integration', function () {
   });
   describe('find', function () {
     describe('in a bucket', function () {
+
       describe('with Read claim', function () {
+        before(function () {
+          member.roles.bucketRoles = [{
+            bucket: bucket._id,
+            role: reader._id
+          }];
+        });
+        after(function () {
+          member.roles = {};
+        });
         describe('when object exists', function () {
-          it('returns array with one entry');
+          var _result;
+          before(function (done) {
+            BBPromise.using(MongoClient.connectAsync(config.get('Hoist.mongo.db'))
+              .disposer(function (connection) {
+                connection.close();
+              }), function (connection) {
+                var db = connection.db('datakey');
+                var collection = BBPromise.promisifyAll(db.collection('live:bucketid:people'));
+                return collection.insertOneAsync({
+                  _id: 'owen.evans',
+                  name: 'owen',
+                  _createdDate: new Date(),
+                  _updatedDate: new Date()
+                });
+              })
+              .then(function () {
+                hoistContext.namespace.run(function () {
+                  setContext().then(function () {
+                    pipeline.find('person', {}).nodeify(function (err, result) {
+                      _result = result;
+                      done(err);
+                    });
+                  });
+                });
+              });
+          });
+          after(function () {
+            return BBPromise.using(MongoClient.connectAsync(config.get('Hoist.mongo.db'))
+              .disposer(function (connection) {
+                connection.close();
+              }), function (connection) {
+                var db = BBPromise.promisifyAll(connection.db('datakey'));
+                return db.dropDatabase();
+              });
+          });
+          it('returns array with one entry', function () {
+            expect(_result.length).to.eql(1);
+            expect(_result).to.contain({
+              name: 'owen',
+              _createdDate: new Date(),
+              _updatedDate: new Date(),
+              _id: 'owen.evans'
+            });
+          });
         });
         describe('when many objects exist', function () {
           it('returns array with many entries object');
@@ -415,13 +466,92 @@ describe('integration', function () {
         });
       });
       describe('without Read claim', function () {
-        it('throws a permission error');
+        var error;
+        before(function (done) {
+          member.roles.mainRole = globalReader._id;
+          hoistContext.namespace.run(function () {
+            setContext().then(function () {
+              pipeline.find('person', {}).nodeify(function (err) {
+                error = err;
+                done();
+              });
+            });
+          });
+        });
+        after(function () {
+          member.roles = {};
+        });
+        it('throws permission error', function () {
+          expect(error.message).to.eql('Current user does not have permission to Read Data');
+        });
       });
     });
     describe('with no bucket', function () {
+      before(function () {
+        var _original = setContext;
+        setContext = function () {
+          return setContext._original().then(function (context) {
+            context.bucket = null;
+            return context;
+          });
+        };
+        setContext._original = _original;
+      });
+      after(function () {
+        setContext = setContext._original;
+      });
       describe('with GlobalRead claim', function () {
+        before(function () {
+          member.roles.mainRole = globalReader._id;
+        });
+        after(function () {
+          member.roles = {};
+        });
         describe('when object exists', function () {
-          it('returns array with one entry');
+          var _result;
+          before(function (done) {
+            BBPromise.using(MongoClient.connectAsync(config.get('Hoist.mongo.db'))
+              .disposer(function (connection) {
+                connection.close();
+              }), function (connection) {
+                var db = connection.db('datakey');
+                var collection = BBPromise.promisifyAll(db.collection('live:global:people'));
+                return collection.insertOneAsync({
+                  _id: 'owen.evans',
+                  name: 'owen',
+                  _createdDate: new Date(),
+                  _updatedDate: new Date()
+                });
+              })
+              .then(function () {
+                hoistContext.namespace.run(function () {
+                  setContext().then(function () {
+                    pipeline.find('person', {}).nodeify(function (err, result) {
+                      _result = result;
+                      done(err);
+                    });
+                  });
+                });
+              });
+          });
+          after(function () {
+            return BBPromise.using(MongoClient.connectAsync(config.get('Hoist.mongo.db'))
+              .disposer(function (connection) {
+                connection.close();
+              }), function (connection) {
+                var db = BBPromise.promisifyAll(connection.db('datakey'));
+                return db.dropDatabase();
+              });
+          });
+          it('returns array with one entry', function () {
+            expect(_result.length).to.eql(1);
+            expect(_result).to.contain({
+              name: 'owen',
+              _createdDate: new Date(),
+              _updatedDate: new Date(),
+              _id: 'owen.evans'
+            });
+          });
         });
         describe('when many objects exist', function () {
           it('returns array with many entries object');
@@ -431,7 +561,26 @@ describe('integration', function () {
         });
       });
       describe('without GlobalRead claim', function () {
-        it('throws permission error');
+        var error;
+        before(function (done) {
+          member.roles.mainRole = reader._id;
+          hoistContext.namespace.run(function () {
+            setContext().then(function () {
+              pipeline.find('person', {
+
+              }).nodeify(function (err) {
+                error = err;
+                done();
+              });
+            });
+          });
+        });
+        after(function () {
+          member.roles = {};
+        });
+        it('throws permission error', function () {
+          expect(error.message).to.eql('Current user does not have permission to Read Global Data');
+        });
       });
     });
   });
